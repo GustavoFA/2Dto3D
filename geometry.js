d0dfaf2bd84d60dc21603151dd3ca93ddc4f45a6
Gustavo B550M <g236249@dac.unicamp.br>
2026-04-12 23:22:52 -0300

    Add point and line functions

--- geometry.js
+++ geometry.js
@@ -14,3 +14,18 @@ export const faces = [
     [4, 5, 6, 7],
     [0, 4], [1, 5], [2, 6], [3, 7]
 ]
+
+export function point(ctx, color, {x, y}, size=20) {
+    const s = size
+    ctx.fillStyle = color
+    ctx.fillRect(x - s/2, y - s/2, s, s)
+}
+
+export function line(ctx, color, p1, p2, thickness=3) {
+    ctx.lineWidth = thickness
+    ctx.strokeStyle = color
+    ctx.beginPath()
+    ctx.moveTo(p1.x, p1.y)
+    ctx.lineTo(p2.x, p2.y)
+    ctx.stroke()
+}
